39753b8a706fa6c07e3f716ebd513a99da681626
Yaryna Sorochuk <yarkasorochuk@gmail.com>
2018-02-07 12:16:53 +0000

    Initial

--- js/main.js
+++ js/main.js
@@ -1,1 +1,54 @@
-alert('Hello World!');
+function myFunction() {
+    if (screen.width < 991.97) {
+        return document.getElementById("p1").innerHTML = 'Join our team and become one<br>of us';
+    }
+}
+myFunction();
+
+
+function changeTeam() {
+    if (screen.width < 991.97) {
+    return document.getElementById("team-collapse").style.display = "none";
+    }
+}
+
+changeTeam();
+
+function changeCollapseTeam() {
+    if (screen.width > 991.98) {
+    return document.getElementById("team-title-sec").style.display = "none";
+    }
+}
+
+changeCollapseTeam();
+
+function teamChange() {
+    if (screen.width > 991.98) {
+    return document.getElementById("team-block").style.display = "none";
+    }
+}
+
+teamChange();
+
+// function navIsFixed() {
+//     if (screen.width > 991.98) {
+//     document.getElementById("nav-fixed").navbar += "sticky";
+//     }
+// }
+
+// navIsFixed();
+
+// window.onscroll = function() {
+//     menuFunction();
+// };
+
+// var navbar = document.getElementById("navbarNav");
+// var sticky = navbar.offsetTop;
+
+// function menuFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky")
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
