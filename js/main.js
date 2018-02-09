97790c283fe71391ae0846b9ba78c9c3930cb707
yarkaya <35272391+yarkaya@users.noreply.github.com>
2018-02-09 17:50:36 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ myFunction();
 
 function changeTeam() {
     if (screen.width < 991.97) {
-    return document.getElementById("team-collapse").style.display = "none";
+        return document.getElementById("team-collapse").style.display = "none";
     }
 }
 
@@ -16,7 +16,7 @@ changeTeam();
 
 function changeCollapseTeam() {
     if (screen.width > 991.98) {
-    return document.getElementById("team-title-sec").style.display = "none";
+        return document.getElementById("team-title-sec").style.display = "none";
     }
 }
 
@@ -24,7 +24,7 @@ changeCollapseTeam();
 
 function teamChange() {
     if (screen.width > 991.98) {
-    return document.getElementById("team-block").style.display = "none";
+        return document.getElementById("team-block").style.display = "none";
     }
 }
 
@@ -51,3 +51,40 @@ teamChange();
 //      navbar.classList.remove("sticky");
 //   }
 //  }
+
+
+
+function initMap() {
+    var uluru = { lat: 49.833514, lng: 24.012306 };
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 17,
+        center: uluru
+    });
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+    });
+}
+
+initMap();
+
+
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById("myBtn").style.display = "block";
+    }
+    else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+topFunction();
+
+// https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_scroll_to_top
